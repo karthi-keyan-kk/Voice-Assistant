9e6ea93ef035667df3ccb0b94858f68879a79459
karthikeyan <112537416+karthi-keyan-kk@users.noreply.github.com>
2023-09-03 20:03:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,13 +1,13 @@
 # To send mail
-senderemail = "karthi.sk1467@gmail.com"
-pwd = "jazfyzjzolhuribo"
+senderemail = "Mail ID"
+pwd = "Password"
 
 
 # Check the current weather
 # rapidapi.com
-key = "1c7b82c554mshfb31d9fc4d74084p146434jsn848a852056ce"
-host = "open-weather13.p.rapidapi.com"
+key = "KEY"
+host = "HOST ID"
 
 
 # corona host
-corona_host = "covid-19-statistics.p.rapidapi.com"
+corona_host = "HOST ID"
